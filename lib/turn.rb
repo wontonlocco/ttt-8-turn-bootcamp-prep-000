f94746d75f19627c42ea80834d1ec000ce22115f
Ed Wright <ew_wrightiii@live.com>
2018-02-11 06:55:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,7 +22,7 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
-def turn(board)
+def turn(board, user_selection)
   
    user_selection.to_i.between?(1, 9) ? input_to_index(user_selection) : turn(board)
   display_board(board)
